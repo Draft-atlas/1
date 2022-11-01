c3a49827ccc5c7facafcdfd7e96fafe32e8546d2
Stanislav <draft.atlas@gmail.com>
2022-11-01 21:38:42 +0300

    начало

---
+++ main.py
@@ -0,0 +1,18 @@
+def read_data_file(file_name):
+    file_lines = []
+    file = open(file_name, 'r')
+    for line in file:
+        file_lines.append(line)
+    return file_lines
+
+
+def split_line(line):
+    splited_line = line.split(';')
+    return splited_line
+
+
+if __name__ == '__main__':
+    stroki_iz_faila = read_data_file('data.csv')
+    for stroka in stroki_iz_faila:
+        razbitaya_stroka = split_line(stroka)
+        print(razbitaya_stroka)
