6ac5690712f9189a2dbe1606f70d9a7275086f1f
Stanislav <draft.atlas@gmail.com>
2022-11-03 19:44:02 +0300

    Проект намбэр 1

--- main.py
+++ main.py
@@ -15,4 +15,8 @@ if __name__ == '__main__':
     stroki_iz_faila = read_data_file('data.csv')
     for stroka in stroki_iz_faila:
         razbitaya_stroka = split_line(stroka)
-        print(razbitaya_stroka)
+        #print(razbitaya_stroka)
+        print(f'Меня зовут {razbitaya_stroka[0]} {razbitaya_stroka[1]}'
+              f',мой пол {razbitaya_stroka[2]},'
+              f'год моего рождения {razbitaya_stroka[3]},'
+              f'можете звонить по номеру {razbitaya_stroka[4]}')
